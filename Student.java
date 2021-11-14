2afa8d738b328ad85892feeac872368bfebc2733
Snigdha Singh <90649383+Snigdha667@users.noreply.github.com>
2021-11-14 22:11:57 +0530

    To display name of student

--- Student.java
+++ Student.java
@@ -1,28 +1,17 @@
 public class Student {
-    String name;
-    double percentage;
-    Student(String n, double p){
-        name = n;
-        percentage = p;
+    String s;
+    Student(){
+        s="Unknown";
     }
-}
-class Test {
+    Student(String s){
+        this.s=s;
+    }
+
     public static void main(String[] args) {
-        Student obj1 = new Student("Riya", 67.77);
-        Student obj2 = new Student("Tiya", 98.90);
-        Student obj3 = new Student("Siya", 78.34);
-        if (obj1.percentage > obj2.percentage) {
-            if (obj1.percentage > obj3.percentage) {
-                System.out.println(obj1.name + " " + obj1.percentage);
-            }
-        }
-        else if(obj2.percentage>obj1.percentage){
-            if(obj2.percentage>obj3.percentage){
-                System.out.println(obj2.name + " " + obj2.percentage);
-            }
-        }
-        else{
-            System.out.println(obj3.name + " " + obj3.percentage);
-        }
+        Student obj = new Student();
+        Student obj1 = new Student("Snigdha");
+        System.out.println(obj.s);
+        System.out.println(obj1.s);
+
     }
 }
