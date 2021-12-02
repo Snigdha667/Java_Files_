d545fcb2590cfbe9077ad6fa4564121f91eace2c
Snigdha Singh <90649383+Snigdha667@users.noreply.github.com>
2021-12-02 23:07:27 +0530

    To stablish connection to mysql database and specify error if unable to connect

--- Display.java
+++ Display.java
@@ -1,20 +1,21 @@
+import java.sql.*;
+
 public class Display {
-    public static void main(String[] args) {
-        Check obj = new Check("No.of objects",+1);
-        System.out.println(obj.i);
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Mysqll", "root", "");
+            Statement s = conn.createStatement();
+            ResultSet rss = s.executeQuery("select * from emp");
+            int i = 0;
+            while (rss.next()) {
+                i++;
+            }
+            if (i > 0) {
+                System.out.println("Connection Stablished Successfully");
+            }
+        }catch(Exception e){
+            System.out.println("Connection could not be established "+e);
+        }
     }
-
-}
-class Check{
-    String s;
-    int i;
-    Check(String s, int j){
-        this.s = s;
-        i=j;
-    }
-    Check obj1 = new Check("Object 1",+1);
-    Check obj2 = new Check("Object 2",+1);
-    Check obj3 = new Check("Object 3",+1);
-    Check obj4 = new Check("Object 4",+1);
-
 }
